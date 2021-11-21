103cf24c4fa61748231dc3951d4a2bbf0e20c894
Slodziak190q <ginalskimateusz889@gmail.com>
2021-11-21 20:47:40 +0100

    Embed LOLZ

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
-bot.login("Twój Token");
+bot.login("OTExNzQ2MjA2NTE3MDQzMjEw.YZl30w.sIGrtKx1rNguSUymbglwe50chfY");
 
 bot.on('ready', (ready) => {
     console.log("Bocik działa!")
-    bot.user.setActivity("Nigas efi yoo")
+    bot.user.setActivity("Made by ench__")
 });
 
 bot.on('message', (message) => {
@@ -14,3 +14,52 @@ bot.on('message', (message) => {
         message.channel.send("Pong (uczegredzie)")
     }
 });
+// Lekcja embed
+bot.on('message', message => {
+    if(message.content === '!embed') {
+        const pomoc = new Discord.MessageEmbed()
+            .setColor("#a8a432")
+            .setTitle("Pomoc drogowa")
+            .setDescription("Opis opis")
+            .addFields(
+            {
+                name: 'Jakas komenda',
+                value: 'COS ONA ROBI',
+                inline: true
+            },
+            {
+                name: 'jakas komenda',
+                value: 'COS TAM',
+                inline: true
+            },
+            {
+                name: 'Jakas komenda',
+                value: 'COS ONA ROBI',
+                inline: true
+            },
+            {
+                name: 'Jakas komenda',
+                value: 'COS ONA ROBI',
+                inline: true
+            },
+            {
+                name: 'Jakas komenda',
+                value: 'COS ONA ROBI',
+                inline: true
+            },    {
+                name: 'Jakas komenda',
+                value: 'COS ONA ROBI',
+                inline: true
+            },
+            {
+                name: 'Jakas komenda',
+                value: 'COS ONA ROBI',
+                inline: true
+            }
+            )
+            .setFooter("Zrobione Przez ench__")
+            .setImage("https://cdn.discordapp.com/attachments/911753077852356622/912066436569108500/Zrzut_ekranu_2021-10-04_183107.png")
+            
+      message.channel.send(pomoc)  
+    } 
+})
